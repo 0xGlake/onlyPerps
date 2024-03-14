8de3a086c0ac01d788219cb9e78d53e9b57b0721
Glake <0xglake@gmail.com>
2024-03-14 20:14:11 +1100

    display y-axis properly

--- main/app/components/FundingRateHeatMap.tsx
+++ main/app/components/FundingRateHeatMap.tsx
@@ -26,24 +26,33 @@ const FundingRateHeatMap: React.FC<Props> = ({ data }) => {
     const width = 800 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
 
-    const svg = d3.select(svgRef.current)
+    const svg = d3
+      .select(svgRef.current)
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    const assets = Object.keys(data[0].aevo_data);
+    const exchanges = Object.keys(data[0]).filter(
+      (key) => key !== 'id' && key !== 'timestamp'
+    );
+    const assets = Object.keys(data[0][exchanges[0]]);
 
-    const yScale = d3.scaleBand()
+    const yDomain = assets.flatMap((asset) =>
+      exchanges.map((exchange) => `${asset}-${exchange}`)
+    );
+
+    const yScale = d3
+      .scaleBand()
       .range([height, 0])
-      .domain(assets)
+      .domain(yDomain)
       .padding(0.05);
 
-    svg.append('g')
-      .call(d3.axisLeft(yScale));
+    svg
+      .append('g')
+      .call(d3.axisLeft(yScale).tickFormat((d) => d.split('-').join(' ')));
 
     // TODO: Create x-axis scale and axis
-
   }, [data]);
 
   return <svg ref={svgRef} />;
