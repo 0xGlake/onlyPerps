ee405591e54d38c948b26311f9800d5746112787
Glake <0xglake@gmail.com>
2024-05-30 14:09:33 +1000

    starter code for API routes for exchanges overview and tokens, will need to do some pre processing me thinks

---
+++ main/app/api/tokens/route.ts
@@ -0,0 +1,16 @@
+import pool from '../../lib/database';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  try {
+    const { rows } = await pool.query(
+    `SELECT *
+    FROM onlyperpstokens
+    ORDER BY id DESC
+    LIMIT 10;`);
+    return NextResponse.json(rows);
+  } catch (error) {
+    console.error('Error fetching data from Neon:', error);
+    return NextResponse.json({ error: 'An error occurred while fetching data from Neon' }, { status: 500 });
+  }
+}
